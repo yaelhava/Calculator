0c50606db4c9cfc1db825005cead377ee0ff9945
yaelhava <yaelhava2@gmail.com>
2024-03-27 10:10:24 +0200

    add form validation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,20 +7,43 @@ import AuthContext from '../AuthContext';
 const Login = () => {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
+    const [error, setError] = useState('');
+    // const [showError, setShowError] = useState(false);
     const navigate = useNavigate();
     const { setUsername: setAuthUsername } = useContext(AuthContext);
 
     const handleSubmit = (e) => {
-        // e.preventDeafault();
+        e.preventDefault();
+
+        if (!username || !email) {
+            setError('Please fill in all fields');
+            // setShowError(true);
+            return;
+        }
+
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailPattern.test(email)) {
+            setError('Please enter a valid email address');
+            // setShowError(true);
+            return;
+        }
+
         setAuthUsername(username);
         navigate("/calculator");
     };
 
 
+    const handleButtonClick = () => {
+        // Clear error message and reset showError state on button click
+        setError('');
+        // setShowError(false);
+    };
+
     return (
         <div>
             <div  className='login-form'>
                 <h2>Login Page</h2>
+                {error && <p style={{ color: 'red' }}>{error}</p>}
                 <form onSubmit={handleSubmit}>
                     <input 
                     type="text" 
@@ -36,7 +59,7 @@ const Login = () => {
                     onChange={(e) => setEmail(e.target.value)} 
                     />
                     <br />
-                    <button type="submit">Login</button>
+                    <button type="submit" onClick={handleButtonClick}>Login</button>
                 </form>
             </div>
         </div>
