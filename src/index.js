54106347b28c50bb7f09748102d64a06dff461af
yaelhava <yaelhava2@gmail.com>
2024-03-27 09:10:24 +0200

    history with redux - works but should fix..

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createStore, configureStore } from 'redux';
+import { createStore } from 'redux';
 import reducer from './reducer';
 import App from './App';
 import "./styles.css"
 // import { BrowserRouter } from 'react-router-dom';
 
-const store = configureStore(reducer);
+const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
